5c5ade2e3eae0f522846985a00cd4f7498e67139
Kryzle <kryzlesayat@gmail.com>
2026-05-03 15:58:38 +0800

    add standard trigonometric functions (sin, cos, tan) with degree-to-radian conversion

--- extended_scientific_calculator.py
+++ extended_scientific_calculator.py
@@ -2,4 +2,10 @@ import math
 from simple_calculator import BasicCalculator
 
 class ScientificCalculator(BasicCalculator):
-    pass
+    #trigo
+    def sin(self, value):
+        return math.sin(math.radians(value))
+    def cos(self, value):
+        return math.cos(math.raians(value))
+    def tan(self, value):
+        return math.tan(math.radians(value))
