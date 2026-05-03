19e0d69bbefa90fe43b3121103dc98d034c712b3
Kryzle <kryzlesayat@gmail.com>
2026-05-03 19:01:13 +0800

    added logic and map input operators for scientific calc

--- calculator_runner_script.py
+++ calculator_runner_script.py
@@ -28,8 +28,8 @@ class CalculatorConsoleInterface(ScientificCalculator):
             one_number_operations = [
                 'sqrt', 'sin', 'cos', 'tan', 'asin', 'acos', 'atan',
                 'sinh', 'cosh', 'tanh', 'asinh', 'acosh', 'atanh',
-                'log', 'ln', 'exp', 'factorial', 'abs',
-                'to_rad', 'to_deg'
+                'log', 'ln', 'exponential', 'factorial', 'abs',
+                'deg_to_rad', 'rad_to_deg'
             ]
             constants = ['pi', 'e']
 
@@ -54,3 +54,46 @@ class CalculatorConsoleInterface(ScientificCalculator):
 
             elif selected_operator in one_number_operations:
                 first_number = float(input("Enter value: "))
+
+                if selected_operator == 'sqrt':
+                    calculation_result = self.square_root(first_number)
+                elif selected_operator == 'sin':
+                    calculation_result = self.sin(first_number)
+                elif selected_operator == 'cos':
+                    calculation_result = self.cos(first_number)
+                elif selected_operator == 'tan':
+                    calculation_result = self.tan(first_number)
+                elif selected_operator == 'asin':
+                    calculation_result = self.asin(first_number)
+                elif selected_operator == 'acos':
+                    calculation_result = self.acos(first_number)
+                elif selected_operator == 'atan':
+                    calculation_result = self.atan(first_number)
+                elif selected_operator == 'sinh':
+                    calculation_result = self.sinh(first_number)
+                elif selected_operator == 'cosh':
+                    calculation_result = self.cosh(first_number)
+                elif selected_operator == 'tanh':
+                    calculation_result = self.tanh(first_number)
+                elif selected_operator == 'asinh':
+                    calculation_result = self.asinh(first_number)
+                elif selected_operator == 'acosh':
+                    calculation_result = self.acosh(first_number)
+                elif selected_operator == 'atanh':
+                    calculation_result = self.atanh(first_number)
+                elif selected_operator == 'log':
+                    calculation_result = self.log(first_number)
+                elif selected_operator == 'ln':
+                    calculation_result = self.ln(first_number)
+                elif selected_operator == 'exponential':
+                    calculation_result = self.exponential(first_number)
+                elif selected_operator == 'factorial':
+                    calculation_result = self.factorial(first_number)
+                elif selected_operator == 'abs':
+                    calculation_result = self.absolute_value(first_number)
+                elif selected_operator == 'deg_to_rad':
+                    calculation_result = self.convert_to_radians(first_number)
+                elif selected_operator == 'rad_to_deg':
+                    calculation_result = self.convert_to_degrees(first_number)
+
+                final_formatted_record = f"{selected_operator}({first_number}) = {calculation_result}"
