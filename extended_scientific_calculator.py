c386409fc0d46215c68392d645497ae3ff1ee191
Kryzle <kryzlesayat@gmail.com>
2026-05-03 15:59:32 +0800

    add inverse trigonometric functions (asin, acos, atan) returning degrees

--- extended_scientific_calculator.py
+++ extended_scientific_calculator.py
@@ -9,3 +9,10 @@ class ScientificCalculator(BasicCalculator):
         return math.cos(math.raians(value))
     def tan(self, value):
         return math.tan(math.radians(value))
+    #inverse trigo
+    def asin(self, value):
+        return math.asin(math.radians(value))
+    def acos(self, value):
+        return math.acos(math.radians(value))
+    def atan(self, value):
+        return math.atan(math.radians(value))
