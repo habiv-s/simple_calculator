09d56d8a841f57d2849696fa82b1d373e8a43699
Kryzle <kryzlesayat@gmail.com>
2026-05-03 18:55:06 +0800

    fixed indentation

--- calculator_runner_script.py
+++ calculator_runner_script.py
@@ -37,17 +37,17 @@ class CalculatorConsoleInterface(ScientificCalculator):
                 first_number = float(input("Enter first number: "))
                 second_number = float(input("Enter second number: "))
 
-                    if selected_operator == '+':
-                        calculation_result = self.add(first_number, second_number)
-                    elif selected_operator == '-':
-                        calculation_result = self.subtract(first_number, second_number)
-                    elif selected_operator == '*':
-                        calculation_result = self.multiply(first_number, second_number)
-                    elif selected_operator == '/':
-                        calculation_result = self.divide(first_number, second_number)
-                    elif selected_operator == '%':
-                        calculation_result = self.modulus(first_number, second_number)
-                    elif selected_operator == '**':
-                        calculation_result = self.power(first_number, second_number)
+                if selected_operator == '+':
+                    calculation_result = self.add(first_number, second_number)
+                elif selected_operator == '-':
+                    calculation_result = self.subtract(first_number, second_number)
+                elif selected_operator == '*':
+                    calculation_result = self.multiply(first_number, second_number)
+                elif selected_operator == '/':
+                    calculation_result = self.divide(first_number, second_number)
+                elif selected_operator == '%':
+                    calculation_result = self.modulus(first_number, second_number)
+                elif selected_operator == '**':
+                    calculation_result = self.power(first_number, second_number)
 
-                    final_formatted_record = f"{first_number} {selected_operator} {second_number} = {calculation_result}"
+                final_formatted_record = f"{first_number} {selected_operator} {second_number} = {calculation_result}"
