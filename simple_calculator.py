137079ca52cc0a9bb4ab9eb410b5c8dd9d616365
Kryzle <kryzlesayat@gmail.com>
2026-05-03 15:31:05 +0800

    added more def logical operators

--- simple_calculator.py
+++ simple_calculator.py
@@ -7,3 +7,19 @@ class BasicCalculator:
 
     def multiply(self, first_number, second_number):
         return first_number * second_number
+
+    def divide(self, first_number, second_number):
+        if second_number == 0:
+            raise ZeroDivisionError("Math Error: Division by Zero")
+        return first_number / second_number
+
+    def modulus(self, first_number, second_number):
+        return first_number % second_number
+
+    def power(self, first_number, second_number):
+        return first_number ** second_number
+
+    def square_root(self, first_number):
+        if first_number < 0:
+            raise ValueError("Cannot calculate square root of negative number.")
+        return first_number ** 0.5
