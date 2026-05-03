47e3971aaf0272ced163bf43ed51ae3d16f920e0
Kryzle <kryzlesayat@gmail.com>
2026-05-03 18:26:38 +0800

    added available operators in my calculator

--- calculator_runner_script.py
+++ calculator_runner_script.py
@@ -9,4 +9,17 @@ class CalculatorConsoleInterface(ScientificCalculator):
             print("               CALCULATOR")
             print("=" * 40)
 
+            print("""               OPERATORS
+
+            two_number_operations: + - * / % **
+
+            one_number_operations:
+            sqrt sin cos tan asin acos atan
+            sinh cosh tanh asinh acosh atanh
+            log ln exponential factorial
+            abs deg_to_rad rad_to_deg
+
+            constants: pi e""")
+            print("=" * 40)
+
             selected_operator = input("Enter operator: ").strip().lower()
