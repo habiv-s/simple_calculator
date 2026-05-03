7a833a99b04b3ea4f999681487afe0c6ead20973
Kryzle <kryzlesayat@gmail.com>
2026-05-03 18:52:18 +0800

    added logic and map input operators for simple calc

--- calculator_runner_script.py
+++ calculator_runner_script.py
@@ -36,3 +36,18 @@ class CalculatorConsoleInterface(ScientificCalculator):
             if selected_operator in two_number_operations:
                 first_number = float(input("Enter first number: "))
                 second_number = float(input("Enter second number: "))
+
+                    if selected_operator == '+':
+                        calculation_result = self.add(first_number, second_number)
+                    elif selected_operator == '-':
+                        calculation_result = self.subtract(first_number, second_number)
+                    elif selected_operator == '*':
+                        calculation_result = self.multiply(first_number, second_number)
+                    elif selected_operator == '/':
+                        calculation_result = self.divide(first_number, second_number)
+                    elif selected_operator == '%':
+                        calculation_result = self.modulus(first_number, second_number)
+                    elif selected_operator == '**':
+                        calculation_result = self.power(first_number, second_number)
+
+                    final_formatted_record = f"{first_number} {selected_operator} {second_number} = {calculation_result}"
