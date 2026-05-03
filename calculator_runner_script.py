76b8fbd7a1bcdbc8b68eced9d17d855edbac1712
Kryzle <kryzlesayat@gmail.com>
2026-05-03 17:08:31 +0800

    define run_calculator method and import sys for flow control

--- calculator_runner_script.py
+++ calculator_runner_script.py
@@ -1,4 +1,7 @@
 from extended_scientific_calculator import ScientificCalculator
+import sys
 
 class CalculatorConsoleInterface(ScientificCalculator):
-    pass
+    def run_calculator(self):
+        """Main interface loop that handles user inputs and manages the logic flow."""
+        pass
